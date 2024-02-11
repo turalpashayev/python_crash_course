8364c4cc50b1d0917a8aee4682a7cb1b62b6dfeb
Tural Pashayev <tural_pasha@turals-mbp.local>
2024-02-12 01:09:11 +0300

    Added list_methods file to remote repo

---
+++ list_methods.py
@@ -0,0 +1,9 @@
+cars = ["bmw", "audi", "toyota", "mercedes"]
+# del cars[0]
+# cars.pop(0)
+# cars.remove("toyota")
+
+# cars.sort()
+# cars = sorted(cars, reverse=True)
+cars.reverse()
+print(cars)
